c4ded7c442b43c1f897bb427697486337a8e9289
Ashish Gupta <ashishkuchiya@gmail.com>
2021-02-25 13:12:15 +0530

    'final-project'

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+      red: {
+        soft3: 'hsl(0, 36%, 70%)',
+        soft2: 'hsl(0, 93%, 68%)',
+        soft1: 'hsl(0, 6%, 24%)',
+        linear1: '135deg, from hsl(0, 0%, 100%), to hsl(0, 100%, 98%)',
+        linear2: '135deg, from hsl(0, 80%, 86%), to hsl(0, 74%, 74%)',
+      }
+    }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
